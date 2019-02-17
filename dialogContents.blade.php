09728e56f805f0177797179ee49a24689b500292
sbhatta5 <47600611+sbhatta5@users.noreply.github.com>
2019-02-17 20:53:34 +0100

    Update dialogContents.blade.php

--- dialogContents.blade.php
+++ dialogContents.blade.php
@@ -10,26 +10,3 @@
 
 </div>
 
-<link rel="stylesheet" type="text/css" href="//cdnjs.cloudflare.com/ajax/libs/cookieconsent2/3.1.0/cookieconsent.min.css" />
-<script src="//cdnjs.cloudflare.com/ajax/libs/cookieconsent2/3.1.0/cookieconsent.min.js"></script>
-<script>
-window.addEventListener("load", function(){
-window.cookieconsent.initialise({
-  "palette": {
-    "popup": {
-      "background": "#237afc"
-    },
-    "button": {
-      "background": "#fff",
-      "text": "#237afc"
-    }
-  },
-  "showLink": false,
-  "theme": "edgeless",
-  "position": "top",
-  "content": {
-    "message": "Diese Website nutzt Cookies, um bestmögliche Funktionalität bieten zu können.",
-    "dismiss": "Verstanden"
-  }
-})});
-</script>
